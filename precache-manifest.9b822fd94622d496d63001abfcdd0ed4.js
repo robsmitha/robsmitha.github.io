f844885792bebf2ffd6b8f6d201e72bd44e0b873
robsmitha <robsmitha94@gmail.com>
2020-05-12 20:14:30 -0400

    Updates

---
+++ precache-manifest.9b822fd94622d496d63001abfcdd0ed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c48cb5823461301dd805bdf07ebd19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2585acf4741036cedf07",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "da9015869c102c80f217",
+    "url": "/static/css/main.9512a8e3.chunk.css"
+  },
+  {
+    "revision": "2585acf4741036cedf07",
+    "url": "/static/js/2.bdd6a2cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bdd6a2cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9015869c102c80f217",
+    "url": "/static/js/main.7d18437f.chunk.js"
+  },
+  {
+    "revision": "d8cd19ac4bf260a82ab3",
+    "url": "/static/js/runtime-main.59e72aa1.js"
+  }
+]);
--- src/Footer.js
+++
@@ -1,8 +0,0 @@
-import Component from './Component.js'
-
-export default class Footer extends Component {
-  constructor(props) {
-    super(props)
-    this.render('./components/footer.html')
-  }
-}
